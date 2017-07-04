69cc4d8dae7adca2bdee82b984c345820ddd7ef5
Steven Wu <steven.wu@flinders.edu.au>
2017-07-04 14:53:23 +0930

    Submit form in CodeIgniter way.

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,9 +1,11 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Login extends CI_Controller {
+class Login extends CI_Controller
+{
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
 
         $this->load->helper('url');
@@ -12,7 +14,22 @@ class Login extends CI_Controller {
         $this->load->library('session');
     }
 
-    public function index() {
+    public function index()
+    {
         $this->load->view('login_form');
+    }
+
+    public function validate()
+    {
+        $this->form_validation->set_rules('username', 'Username');
+        $this->form_validation->set_rules('password', 'Password', 'min_length[8]',
+            array('required' => 'You must provide a %s.')
+        );
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view('login_error');
+        } else {
+            $this->load->view('login_success');
+        }
     }
 }
--- application/views/login_form.php
+++ application/views/login_form.php
@@ -1,5 +1,5 @@
 <?php
-    defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
 <!DOCTYPE html>
@@ -8,20 +8,18 @@
     <meta charset="utf-8">
     <title>Welcome to Quit Smoking Application</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/custom.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/custom.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 
 <body
-<div class="wrapper">
-    <form class="form-signin">
-        <h2 class="form-signin-heading">Please login</h2>
-        <input type="text" class="form-control" name="username" placeholder="Email Address" required="" autofocus=""/>
-        <input type="password" class="form-control" name="password" placeholder="Password" required=""/>
-        <label class="checkbox">
-            <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe"> Remember me
-        </label>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
-    </form>
+<div>
+    <?php echo form_open('login/validate', 'class="form-signin"'); ?>
+    <h2 class="form-signin-heading">Please login</h2>
+    <input type="text" class="form-control" name="username" placeholder="Email Address" required="" autofocus=""/>
+    <input type="password" class="form-control" name="password" placeholder="Password" required=""/>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+    <?php echo form_close(); ?>
 </div>
 </body>
 </html>
